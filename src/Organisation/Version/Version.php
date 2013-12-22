213f7c5d0ea336646c9a3a93d971caa1c4ee09a6
japaveh <info@japaveh.nl>
2013-12-22 22:39:47 +0100

    Update on the status

--- src/Organisation/Version/Version.php
+++ src/Organisation/Version/Version.php
@@ -5,7 +5,7 @@
  * @category    Organisation
  * @package     Version
  * @author      Johan van der Heide <johan.van.der.heide@itea3.org>
- * @copyright   Copyright (c) 2004-2014 Debranova (http://debranova.org)
+ * @copyright   Copyright (c) 2004-2014 ITEA Office (http://itea3.org)
  */
 namespace Organisation\Version;
 
