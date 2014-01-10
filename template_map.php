c2be2240c09bc7c21a3c6321adc1db0bfa17e7ab
japaveh <info@japaveh.nl>
2014-01-10 13:02:22 +0100

    Update on the organisation

--- template_map.php
+++ template_map.php
@@ -6,8 +6,8 @@ return array(
     'organisation/partial/entity/organisation'          => __DIR__ . '//view/organisation/partial/entity/organisation.twig',
     'organisation/partial/form/organisation'            => __DIR__ . '//view/organisation/partial/form/organisation.twig',
     'organisation/partial/list/article'                 => __DIR__ . '//view/organisation/partial/list/article.twig',
-    'organisation/partial/list/organisation'            => __DIR__ . '//view/organisation/partial/list/organisation.twig',
     'organisation/partial/list/organisation-search'     => __DIR__ . '//view/organisation/partial/list/organisation-search.twig',
+    'organisation/partial/list/organisation'            => __DIR__ . '//view/organisation/partial/list/organisation.twig',
     'organisation/partial/list/project'                 => __DIR__ . '//view/organisation/partial/list/project.twig',
     'organisation/partial/pagination-control'           => __DIR__ . '//view/organisation/partial/pagination-control.twig',
 );
