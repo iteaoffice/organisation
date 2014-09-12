78480c6c3dce6b90f4a4c2768cf056f91ed1ceeb
Andre Hebben <andre.hebben@zenesys.nl>
2014-09-12 09:06:19 +0200

    update

---
+++ config/module.option.organisation.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Organisation Options
+ *
+ * If you have a ./config/autoload/ directory set up for your project, you can
+ * drop this config file in it and change the values as you wish.
+ */
+$options = array(
+
+);
+/**
+ * You do not need to edit below this line
+ */
+return array(
+    'organisation-option' => $options,
+);
