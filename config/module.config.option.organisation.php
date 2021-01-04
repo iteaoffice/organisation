5377bcbd6da75f461e89ed26923c1736b4fa7c39
Johan van der Heide <info@japaveh.nl>
2021-01-04 09:29:29 +0100

    copyright updates

--- config/module.config.option.organisation.php
+++ config/module.config.option.organisation.php
@@ -1,6 +1,14 @@
 <?php
 
 /**
+ * ITEA Office all rights reserved
+ *
+ * @author      Johan van der Heide <johan.van.der.heide@itea3.org>
+ * @copyright   Copyright (c) 2021 ITEA Office (https://itea3.org)
+ * @license     https://itea3.org/license.txt proprietary
+ */
+
+/**
  * Organisation Options
  *
  * If you have a ./config/autoload/ directory set up for your project, you can
